d6360763ba317d3ccfb82ebaf7cb82d06ec31cdc
protarie <protarie@gmail.com>
2023-10-07 18:09:27 +0900

    초기 커밋.

---
+++ BOJ_27332/main.cpp
@@ -0,0 +1,47 @@
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+using pll = pair<ll, ll>;
+using vll = vector<pll>;
+
+int main() {
+	ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	int a, b;
+	cin >> a >> b;
+
+	auto v = a + 7 * b;
+
+	if (v <= 30) {
+		cout << 1;
+	}
+	else {
+		cout << 0;
+	}
+
+	return 0;
+}
